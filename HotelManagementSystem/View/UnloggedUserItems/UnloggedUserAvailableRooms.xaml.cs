c8d307d1e5ed25977f0e113865a9b5ef96e88cc5
Bolba-Mateescu Andrei-Ioan <andrei_bolba@yahoo.com>
2022-05-24 06:12:17 +0300

    Display room woth price in that limited period of time

---
+++ HotelManagementSystem/View/UnloggedUserItems/UnloggedUserAvailableRooms.xaml.cs
@@ -0,0 +1,37 @@
+﻿using HotelManagementSystem.ViewModel.UnloggedUserItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem.View.UnloggedUserItems
+{
+    /// <summary>
+    /// Interaction logic for UnloggedUserAvailableRooms.xaml
+    /// </summary>
+    public partial class UnloggedUserAvailableRooms : UserControl
+    {
+        public UnloggedUserAvailableRooms()
+        {
+            UnloggedUserAvailableRoomsVM.start = DateTime.Now;
+            UnloggedUserAvailableRoomsVM.end = DateTime.Now;
+            InitializeComponent();
+        }
+        public UnloggedUserAvailableRooms(DateTime start, DateTime end)
+        {
+            UnloggedUserAvailableRoomsVM.start = start;
+            UnloggedUserAvailableRoomsVM.end = end;
+            InitializeComponent();
+        }
+    }
+}
